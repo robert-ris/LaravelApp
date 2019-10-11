da4a3f66be1a3b0659265caa7251a522e5cefa50
Isacescu Robert <isacescu.robert@gmail.com>
2019-10-11 14:54:26 +0300

    Comm

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md col-md-offset02">
+            <div class="panel panel-default">
+                <div class="panel-deading"><h3>Dashboard</h3></div>
+                <div class="panel-body">
+                    <a href="posts/create" class="btn btn-primary">Create Posts</a>
+                    <h3>Your Blog Posts</h3>
+                    @if(count($posts) > 0)
+                        <table class="table table-striped">
+                            <tr>
+                                <th>Title</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                            @foreach($posts as $post)
+                            <tr>
+                                <td>{{$post->title}}</td>
+                                <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></td>
+                                <td>
+                                    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                                        {{Form::hidden('_method', 'DELETE')}}
+                                        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                                    {!!Form::close()!!}
+                                </td>
+                            </tr>
+                            @endforeach
+                        </table>
+                    @else
+                        <p>You have no posts</p>    
+                    @endif        
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
